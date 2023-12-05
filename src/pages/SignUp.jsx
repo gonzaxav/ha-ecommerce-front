a18fce1ce3c59dbba87e63fd96b497a02e08f167
melirebour <145775507+melirebour@users.noreply.github.com>
2023-12-05 11:19:03 -0300

    signUp css

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -8,6 +8,8 @@ import { useNavigate, Link } from "react-router-dom";
 function SignUp() {
   const [newName, setNewName] = useState("");
   const [newLastName, setNewLastName] = useState("");
+  const [newAddress, setNewAddress] = useState("");
+  const [newPhone, setNewPhone] = useState("");
   const [newEmail, setNewEmail] = useState("");
   const [newPassword, setNewPassword] = useState("");
   const navigate = useNavigate();
@@ -29,84 +31,115 @@ function SignUp() {
   return (
     <>
       <MyNavbar />
-      <section className="section-signUp cream">
+      <section className="section-signUp white">
         <div className="container d-flex align-items-center justify-content-center vh-100">
           <div className="row row-form">
             <div className="col-12 ">
               <h1 className="signup-title">Crear una cuenta</h1>
-
-              <form onSubmit={createClient}>
-                <div className="mb-3">
-                  <label
-                    hidden
-                    htmlFor="firstname"
-                    className="form-label"
-                  ></label>
-                  <input
-                    className="form-control"
-                    type="text"
-                    placeholder="Nombre"
-                    id="firstname"
-                    value={newName}
-                    onChange={(e) => setNewName(e.target.value)}
-                    required
-                    name="firstname"
-                  />
-                </div>
-                <div className="mb-3">
-                  <label hidden htmlFor="lastname"></label>
-                  <input
-                    className="form-control"
-                    type="text"
-                    placeholder="Apellido"
-                    id="lastname"
-                    value={newLastName}
-                    onChange={(e) => setNewLastName(e.target.value)}
-                    required
-                    name="lastname"
-                  />
-                </div>
-                <div className="mb-3">
-                  <label hidden htmlFor="email"></label>
-                  <input
-                    className="form-control"
-                    type="email"
-                    placeholder="Email"
-                    id="email"
-                    value={newEmail}
-                    onChange={(e) => setNewEmail(e.target.value)}
-                    required
-                    name="email"
-                  />
-                </div>
-
-                <div className="mb-3">
-                  <label hidden htmlFor="password"></label>
-                  <input
-                    className="form-control"
-                    type="password"
-                    placeholder="Contraseña"
-                    id="password"
-                    value={newPassword}
-                    onChange={(e) => setNewPassword(e.target.value)}
-                    required
-                    name="password"
-                  />
-                </div>
-                <div className=" d-grid col-6 mx-auto mb-3  d-flex">
-                  <button
-                    className="btn-registro btn btn-secondary "
-                    type="submit"
-                  >
-                    Regístrate
-                  </button>
-                  <div>
-                    <button className="btn-login btn btn-secondary ">
-                      ingresar
-                    </button>
+              <div className="div-form ">
+                <form onSubmit={createClient}>
+                  <div className="mb-3">
+                    <label
+                      hidden
+                      htmlFor="firstname"
+                      className="form-label"
+                    ></label>
+                    <input
+                      className="form-control form-field"
+                      type="text"
+                      placeholder="Nombre"
+                      id="firstname"
+                      value={newName}
+                      onChange={(e) => setNewName(e.target.value)}
+                      required
+                      name="firstname"
+                    />
                   </div>
-                </div>
-              </form>
+                  <div className="mb-3">
+                    <label hidden htmlFor="lastname"></label>
+                    <input
+                      className="form-control form-field"
+                      type="text"
+                      placeholder="Apellido"
+                      id="lastname"
+                      value={newLastName}
+                      onChange={(e) => setNewLastName(e.target.value)}
+                      required
+                      name="lastname"
+                    />
+                  </div>
+
+                  <div className="mb-3">
+                    <label hidden htmlFor="email"></label>
+                    <input
+                      className="form-control form-field"
+                      type="email"
+                      placeholder="Email"
+                      id="email"
+                      value={newEmail}
+                      onChange={(e) => setNewEmail(e.target.value)}
+                      required
+                      name="email"
+                    />
+                  </div>
+
+                  <div className="mb-3">
+                    <label hidden htmlFor="address"></label>
+                    <input
+                      className="form-control form-field"
+                      type="address"
+                      placeholder="Dirección"
+                      id="address"
+                      value={newAddress}
+                      onChange={(e) => setNewAddress(e.target.value)}
+                      required
+                      name="address"
+                    />
+                  </div>
+
+                  <div className="mb-3">
+                    <label hidden htmlFor="phone"></label>
+                    <input
+                      className="form-control form-field"
+                      type="phone"
+                      placeholder="Teléfono"
+                      id="phone"
+                      value={newPhone}
+                      onChange={(e) => setNewPhone(e.target.value)}
+                      required
+                      name="phone"
+                    />
+                  </div>
+
+                  <div className="mb-3">
+                    <label hidden htmlFor="password"></label>
+                    <input
+                      className="form-control form-field"
+                      type="password"
+                      placeholder="Contraseña"
+                      id="password"
+                      value={newPassword}
+                      onChange={(e) => setNewPassword(e.target.value)}
+                      required
+                      name="password"
+                    />
+                  </div>
+                  <div className=" div-btn d-grid col-6  mb-3  d-flex">
+                    <button
+                      className="btn-registro btn btn-ligth "
+                      type="submit"
+                    >
+                      Regístrate
+                    </button>
+                    <div>
+                      
+                      <button className="btn-login btn btn-ligth ">
+                        ingresar
+                      </button>
+                    </div>
+                  </div>
+                </form>
+              </div>
             </div>
           </div>
         </div>
