249cedfb0da11fb35299b43a5c2e956e51bf3991
AlitoZzz <alejonegreira@gmail.com>
2023-12-11 13:02:57 -0300

    Persistencia

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,16 @@ import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import { Provider } from "react-redux";
-import store from "./redux/storeConfig.js";
+import { store, persistor } from "./redux/storeConfig.js";
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
         <App />
+        </PersistGate>
       </Provider>
     </BrowserRouter>
   </React.StrictMode>
--- src/redux/storeConfig.js
+++ src/redux/storeConfig.js
@@ -1,12 +1,32 @@
 import {configureStore} from "@reduxjs/toolkit";
 import orderReducer from "./orderSlice";
 import clientReducer from "./clientSlice";
+import {
+    persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER
+  } from "redux-persist";
+  import storage from "redux-persist/lib/storage";
+  import { combineReducers } from "redux";
+
+  const rootReducer = combineReducers({
+    order: orderReducer,
+    client: clientReducer
+  });
+
+  const persistConfig = { key: "root",  storage };
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 
 const store = configureStore({
-    reducer:{
-        order: orderReducer,
-        client: clientReducer
-    }
-})
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+  });
 
-export default store;
+  const persistor = persistStore(store);
+  
+
+export { store, persistor }
