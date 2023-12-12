3c1393ce96e6aecc75bc0417d050699534f189f4
melirebour <145775507+melirebour@users.noreply.github.com>
2023-12-12 14:42:08 -0300

    logos checkout

--- src/pages/CheckOut.jsx
+++ src/pages/CheckOut.jsx
@@ -4,16 +4,24 @@ import "./CheckOut.css";
 import MyNavbar from "../components/MyNavbar";
 import Footer from "../components/Footer";
 import Card from "react-bootstrap/Card";
-import totalProducts from "../components/CartProduct";
-import totalPrice from "../components/CartProduct";
+//import totalProducts from "../components/CartProduct";
+//import totalPrice from "../components/CartProduct";
 import { useState } from "react";
+import Form from "react-bootstrap/Form";
+import visa from "../img/logos/Visa-pequeño.svg";
+import santander from "../img/logos/santander-logo-15.png";
+import paypal from "../img/logos/paypal-logo-2-1.png";
+import Mastercard from "../img/logos/Mastercard-pequeño.svg";
+import Master from "../img/logos/Maestro-pequeño.svg";
+import Amex from "../img/logos/AmericanExpress.svg";
+import Brou from "../img/logos/brou.png";
 
 import { useDispatch } from "react-redux";
 import axios from "axios";
 
 function CheckOut() {
   const client = useSelector((state) => state.client);
-  const order = useSelector((state)=> state.order);
+  const order = useSelector((state) => state.order);
   const location = useLocation();
 
   const [newEmail, setNewEmail] = useState("");
@@ -24,9 +32,10 @@ function CheckOut() {
   const [newTown, setNewTown] = useState("");
   const [newPostcode, setNewPostcode] = useState("");
   const [newCountry, setNewCountry] = useState("");
-  const [newPayPal, setNewPayPal] = useState("");
+  //const [newCardNumber, setNewCardNumber] = useState("");
+  //const [newNameOfOwner, setNewNameOfOwner] = useState("");
+  //const [newExpirationDate, setNewExpirationDate] = useState("");
 
-  
   const dispatch = useDispatch();
 
   const totalPrice = order.reduce((acumulator, currentProduct) => {
@@ -38,7 +47,6 @@ function CheckOut() {
     (acumulator, currentProduct) => acumulator + currentProduct.qty,
     0
   );
-
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -54,8 +62,6 @@ function CheckOut() {
           Authorization: `Bearer ${client}`,
         },
       });
-      //dispatch(login(response.data))
-      //navigate("/");
     };
     createClientPay();
   };
@@ -76,92 +82,104 @@ function CheckOut() {
               Todos los campos son obligatorios
             </p>
             <form onSubmit={handleSubmit}>
-              <label hidden htmlFor="Email" className="form-label"></label>
+              <label htmlFor="Email" className="form-label">
+                Email
+              </label>
               <input
                 className="form-control form-field field-margin"
                 type="text"
-                placeholder="Email"
+                
                 id="email"
                 value={newEmail}
                 onChange={(e) => setNewEmail(e.target.value)}
                 required
                 name="email"
               />
-              <label hidden htmlFor="firstname" className="form-label"></label>
+              <label htmlFor="firstname" className="form-label">
+                Nombre
+              </label>
               <input
                 className="form-control form-field field-margin"
                 type="text"
-                placeholder="Nombre"
+                
                 id="firstname"
                 value={newFirstname}
                 onChange={(e) => setNewFirstname(e.target.value)}
                 required
                 name="firstname"
               />
-              <label hidden htmlFor="lastname" className="form-label"></label>
+              <label htmlFor="lastname" className="form-label">
+                Apellido
+              </label>
               <input
                 className="form-control form-field field-margin"
                 type="text"
-                placeholder="Apellido"
+                
                 id="lastname"
                 value={newLastname}
                 onChange={(e) => setNewLastname(e.target.value)}
                 required
                 name="lastname"
               />
-              <label hidden htmlFor="telephone" className="form-label"></label>
+              <label htmlFor="telephone" className="form-label">
+                Teléfono
+              </label>
               <input
                 className="form-control form-field field-margin"
                 type="text"
-                placeholder="Teléfono"
+                
                 id="telephone"
                 value={newTelephone}
                 onChange={(e) => setNewTelephone(e.target.value)}
                 required
                 name="telephone"
               />
-              <label
-                hidden
-                htmlFor="deliveryAddress"
-                className="form-label"
-              ></label>
+              <label htmlFor="deliveryAddress" className="form-label">
+                Dirección de envío
+              </label>
               <input
                 className="form-control form-field field-margin"
                 type="text"
-                placeholder="Dirección de envío"
+                
                 id="deliveryAddress"
                 value={newDeliveryAddress}
                 onChange={(e) => setNewDeliveryAddress(e.target.value)}
                 required
                 name="deliveryAddress"
               />
-              <label hidden htmlFor="town" className="form-label"></label>
+              <label htmlFor="town" className="form-label">
+                Departamento
+              </label>
               <input
                 className="form-control form-field field-margin"
                 type="text"
-                placeholder="Departamento"
+                
                 id="town"
                 value={newTown}
                 onChange={(e) => setNewTown(e.target.value)}
                 required
                 name="town"
               />
-              <label hidden htmlFor="postcode" className="form-label"></label>
+              <label htmlFor="postcode" className="form-label">
+                Código postal
+              </label>
               <input
                 className="form-control form-field field-margin"
                 type="text"
-                placeholder="Código postal"
+                
                 id="postcode"
                 value={newPostcode}
                 onChange={(e) => setNewPostcode(e.target.value)}
                 required
                 name="postcode"
               />
-              <label hidden htmlFor="country" className="form-label"></label>
+              <label htmlFor="country" className="form-label label-checkout">
+                País
+              </label>
               <input
                 className="form-control form-field field-margin"
                 type="text"
-                placeholder="País"
+                
                 id="country"
                 value={newCountry}
                 onChange={(e) => setNewCountry(e.target.value)}
@@ -174,28 +192,99 @@ function CheckOut() {
           <div className="col-md-12 col-lg-6">
             <div className="div-pago">
               <h3 className="metodo-titulo">Método de pago</h3>
+              <div >
+                
+                <input
+                  className="check"
+                  id="Transferencia"
+                  type="radio"
+                  name="forma-de-pago"
+                  value={"t"}
+                />
+                <label htmlFor="Transferencia">Transferencia bancaria</label>
+                
+                <div className="bancos ">
+                  <img className="santander-logo" src={santander} alt="logo Santander" />
+                  <img className="brou-logo" src={Brou} alt="logo Brou" />
+                </div>
+              </div>
+              <div >
+                <input
+                  className="check"
+                  id="Tarjeta-de-credito "
+                  type="radio"
+                  name="forma-de-pago"
+                  value={"t-c"}
+                />
+                <label htmlFor="Tarjeta-de-credito">Tarjeta de credito</label>
+                <div className="tarjetas">
+                  <img className="visa-logo" src={visa} alt="Visa" />
+                  <img src={Master} alt="Mster" />
+                  <img src={Mastercard} alt="Mastercard" />
+                  <img src={Amex} alt="Amex" />
+                </div>  
+              </div>
+              <div >
+                <input
+                  className="check"
+                  id="PayPal"
+                  type="radio"
+                  name="forma-de-pago"
+                  value={"p"}
+                />
+                <label htmlFor="PayPal">PayPal</label>
+                <div className="paypal-logo">
+                <img className="paypal-img" src={paypal} alt="" />
+                </div>
+              </div>
 
-              <label hidden htmlFor="Card" className="form-label"></label>
+              <label htmlFor="cardNumber" className="form-label label-checkout">
+                Nº de tarjeta
+              </label>
               <input
                 className="form-control form-field field-margin"
                 type="text"
-                placeholder="Tarjeta"
-                id="Card"
-                //value="Card"
-                name="Card"
+                
+                id="cardNumber"
+                //value={newCardNumber}
+                //onChange={(e) => setNewCountry(e.target.value)}
+                //required
+                name="cardNumeber"
               />
-              <label hidden htmlFor="payPal" className="form-label"></label>
+              <label
+                htmlFor="nameOfOwner"
+                className="form-label label-checkout"
+              >
+                Nombre del titular
+              </label>
               <input
                 className="form-control form-field field-margin"
                 type="text"
-                placeholder="PayPal"
-                id="payPal"
-                value={newPayPal}
-                onChange={(e) => setNewPayPal(e.target.value)}
-                required
-                name="payPal"
+                
+                id="nameOfOwner"
+                //value={newNameOfOwner}
+                //onChange={(e) => setNewCountry(e.target.value)}
+                //required
+                name="nameOfOwner"
+              />
+              <label
+                htmlFor="expirationDate"
+                className="form-label label-checkout"
+              >
+                Fecha de expiración
+              </label>
+              <input
+                className="form-control form-field field-margin"
+                type="text"
+                
+                id="expirationDate"
+                //value={newExpirationDate}
+                //onChange={(e) => setNewCountry(e.target.value)}
+                //required
+                name="expirationDate"
               />
             </div>
+
             <div className="div-resumen">
               <Card className="card-resumen">
                 <Card.Body>
@@ -213,7 +302,9 @@ function CheckOut() {
             </div>
             <div>
               <div className="btn-pago">
-                <button className="btn-pagar" type="submit">Pagar</button>
+                <button className="btn-pagar" type="submit">
+                  Pagar
+                </button>
               </div>
             </div>
           </div>
